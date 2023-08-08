13eb86cb1cd29dd84c8b24307645a6934e2df170
aayushk22 <ayushkumar22.ak@gmail.com>
2023-08-09 00:50:35 +0530

    1st attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+//        obj.name = "Aayush";
+//        System.out.println(obj.name);
+        //error message: name has private access in com.driver.RWOnly
+
+        obj.setName("Aayush");
+        System.out.println(obj.getName());
+    }
 }
